72a0a03430552e29f5a61ebd3b4418edfb40d3ac
krishnamohan <krishnamohan.py@gmail.com>
2020-06-19 21:29:56 +0530

    FSJ0520A119

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,22 @@
+package controller;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main
+{
+	public static void main(String [] args) throws IOException, SQLException
+	{
+		SkillDAO skilldao =new SkillDAO();
+		ArrayList<Skill>list=  (ArrayList<Skill>) skilldao.listAllSkills();
+//		System.out.println(((Object) list).getSkill());
+		for(Skill s :list)
+		{
+			System.out.println(s.getSkillName()+" "+s.getSkilld());
+		}
+	}
+	}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,37 @@
+package dao;
 
+import java.beans.Statement;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+{
+	static List<Skill> list=new ArrayList<Skill>();
+	public List< Skill >listAllSkills() throws IOException, SQLException
+	{
+		Connection con =ConnectionManager.getConnection();
+		
+	          String skillquery="select * from SKILL order by NAME";
+		PreparedStatement st = con.prepareStatement(skillquery);
+        
+		ResultSet rs = st.executeQuery();
+		rs.next();
+//		System.out.println(rs.next());
+//		while(rs.next())
+//		{
+		Skill skill =new Skill(rs.getLong(1),rs.getString(2));
+		
+		list.add(skill);
+//		}
+		return list;
+	}
+	
+	}
